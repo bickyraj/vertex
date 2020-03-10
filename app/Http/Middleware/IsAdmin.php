82b4660edef347b87d1a07e2f2515d908ab23c25
Bicky <bickyraj.kayastha@innologysolution.com>
2020-03-10 23:13:09 +0545

    Updates

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check() &&  Auth::user()->role_id === 1) {
+        if (Auth::check() && (int)Auth::user()->role_id === 1) {
             return $next($request);
         }
         return redirect()->route('admin.login');
